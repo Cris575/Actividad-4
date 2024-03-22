b3691d845bc4c65cc54ee6e36906ceec32fcf751
Cristian Gonzalez Herrera <cg691539@gmail.com>
2024-03-22 08:51:53 -0600

    commit

--- src/components/item.js
+++ src/components/item.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { View, StyleSheet, Text } from "react-native";
 
-const Item = ({ title }) => (
-  <View style={styles.item}>
-    <Text style={styles.title}>{title}</Text>
-  </View>
-);
+const Item = ({ title }) => {
+  return (
+    <View style={styles.item}>
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   item: {
